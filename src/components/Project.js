47278eee3da4693370521a408a1da8be4c6200f3
LionelGareca95 <lionelgareca@gmail.com>
2022-12-25 14:08:37 -0300

    project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import afilados from '../images/afilados.webp';
+import SayQuality from '../images/sayquality.webp' 
+import constructora from '../images/constructora.webp' 
 import { BsBoxArrowUpRight } from 'react-icons/bs';
 import { Link } from "gatsby";
 import {FcBriefcase} from 'react-icons/Fc'
+
 
 const ContainerProject = styled.div`
     padding: 2rem 0;
@@ -68,16 +71,17 @@ const CardProject = styled.div`
   const Container = styled.div`
     background-color: #302F2E;
   `
- const ProjectImg = styled.img.attrs({
-  src: `${afilados}`
-})`
 
-`;
 const Parrafo = styled.div`
 max-width: 600px;
   padding: 1rem;
   margin: 0 auto;
   text-align: justify;
+
+  p {
+    line-height: 25px;
+    font-size: 1.2rem;
+  }
 `
 
 const Project = () => {
@@ -89,10 +93,10 @@ const Project = () => {
       <ContainerCard>
         <h1>Afilados Rosario</h1>
       <CardProject className='card'>
-            <ProjectImg loading='lazy' />
-          <Link to='https://www.afiladosrosario.com.ar/'>
+            <img src={afilados} loading='lazy' alt='Afilados Rosario'/>
+          <a target="_blank" rel="noopener noreferrer" href='https://www.afiladosrosario.com.ar/'>
             <Button className='info'><BsBoxArrowUpRight/> View </Button>   
-          </Link>
+          </a>
       </CardProject>
       <Parrafo>
         <p>Se han utilizado las tecnologias de HTML-CSS-Javascript-Sass
@@ -104,18 +108,15 @@ const Project = () => {
       </ContainerCard>
 
       <ContainerCard>
-        <h1>Afilados Rosario</h1>
+        <h1>Say Quality</h1>
       <CardProject className='card'>
-          <ProjectImg loading='lazy' />
-          <Link to='https://www.afiladosrosario.com.ar/'>
+          <img src={SayQuality} loading='lazy' alt='Say Quality' />
+          <a target="_blank" rel="noopener noreferrer" href='https://www.afiladosrosario.com.ar/'>
             <Button className='info'><BsBoxArrowUpRight/> View </Button>   
-          </Link>
+          </a>
       </CardProject>
       <Parrafo>
-        <p>Se han utilizado las tecnologias de HTML-CSS-Javascript-Sass
-          Se ha utilizado una libreria llamada Swiper Js que incluye
-          una galeria de imagenes con los distintos productos. Contiene enlaces a redes sociales y un mapa con la localizacion.
-          Es una empresa familiar que ofrece servicios de afilados. al estar vinculado a Google Mi negocio, aumento exponencialmente su clientela.
+        <p>El proyecto fue grupal y fuimos integrando los cambios en github. Contó con un front-end (yo) - Full-Stack - Back-end - Project Manager y Tester Qa. Se han utilizado las tecnologias de Sass, React (vite) y peticiones fetch nativas y validaciones con Vainilla JavaScript para el Front-End. En tanto el Back se utilizo SQL y Jason Web Token para guardar la info del login en el Local Storage.  
         </p>
       </Parrafo>
       </ContainerCard>
@@ -123,10 +124,10 @@ const Project = () => {
       <ContainerCard>
         <h1> Afilados Rosario</h1>
       <CardProject className='card'>
-          <ProjectImg loading='lazy' />
-          <Link to='https://www.afiladosrosario.com.ar/'>
+          <img loading='lazy' src={constructora} alt='Constructora'/>
+          <a target="_blank" rel="noopener noreferrer" href='https://constructora85.netlify.app/'>
             <Button className='info'><BsBoxArrowUpRight/> View </Button>   
-          </Link>
+          </a>
       </CardProject>
       <Parrafo>
         <p>Se han utilizado las tecnologias de HTML-CSS-Javascript-Sass
@@ -140,10 +141,10 @@ const Project = () => {
       <ContainerCard>
         <h1 className='titulo'>Afilados Rosario</h1>
       <CardProject className='card'>
-            <ProjectImg loading='lazy' />
-          <Link to='https://www.afiladosrosario.com.ar/'>
+            <img loading='lazy' alt='asd' />
+          <a target="_blank" rel="noopener noreferrer" href='https://www.afiladosrosario.com.ar/'>
             <Button className='info'><BsBoxArrowUpRight/> View </Button>   
-          </Link>
+          </a>
       </CardProject>
       <Parrafo>
         <p>Se han utilizado las tecnologias de HTML-CSS-Javascript-Sass
